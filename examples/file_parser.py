ee60665cfbf941584d595597f423d09b717601ba
Mark Bradford <markb424@gmail.com>
2014-09-30 00:41:32 +0000

    Used getter

--- examples/file_parser.py
+++ examples/file_parser.py
@@ -26,22 +26,22 @@ class Parser():
                 filename = os.path.join(root, name)
                 extension = os.path.splitext(filename)[1].replace('.', '')
 
-                self.stats['total_files'] += 1
-                self.stats['total_size'] += os.path.getsize(filename) / (1024 * 1024.0)
+                # self.stats['total_files'] += 1
+                # self.stats['total_size'] += os.path.getsize(filename) / (1024 * 1024.0)
 
                 # Only get files that match the specified extension
                 if str(extension) == str(self.extension):
                     self.file_list.append(filename)
-                    self.stats['filtered_files'] += 1
-                    self.stats['filtered_size'] += os.path.getsize(filename) / (1024 * 1024.0)
+                    # self.stats['filtered_files'] += 1
+                    # self.stats['filtered_size'] += os.path.getsize(filename) / (1024 * 1024.0)
 
             # Count number of directories
             for dir_item in dirs:
                 self.stats['directories'] += 1
 
         # Collect stats about parsed files
-        self.stats['total_size'] = "%d Mb" % round(self.stats['total_size'], 2)
-        self.stats['filtered_size'] = "%d Mb" % round(self.stats['filtered_size'], 2)
+        # self.stats['total_size'] = "%d Mb" % round(self.stats['total_size'], 2)
+        # self.stats['filtered_size'] = "%d Mb" % round(self.stats['filtered_size'], 2)
 
     def get_file_list(self):
         """
@@ -52,3 +52,9 @@ class Parser():
 
     def get_stats(self):
         return self.stats
+
+if __name__ == "__main__":
+    instance = Parser('/', extension='mp3')
+    instance.populate_file_list()
+    print instance.get_file_list()
+
